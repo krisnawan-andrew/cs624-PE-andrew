207b3b3578e49700fa432f30472fd370d4f4cac3
krisnawan-andrew <krisnawanandrew@cityuniversity.edu>
2025-05-17 14:28:18 -0700

    After hos06

--- pe04/app/(tabs)/App.js
+++ pe04/app/(tabs)/App.js
@@ -1,39 +1,97 @@
+import update from 'immutability-helper';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+const userImage = require('../../assets/images/user.png');
+
+const data = [{
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great JavaScript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+}]
+
+const ProfileCard = (props) => {
+    const { image, name, occupation, description, onPress, showThumbnail } = props;
+    let containerStyles = [styles.cardContainer];
+
+    if (showThumbnail) {
+        containerStyles.push(styles.cardThumbnail);
+    }
+
+    return (
+        <TouchableHighlight onPress={onPress}>
+            <View style={[containerStyles]}>
+                <View style={styles.cardImageContainer}>
+                    <Image style={styles.cardImage} source={image}/>
+                </View>
+                <View>
+                    <Text style={styles.cardName}>
+                        {name}
+                    </Text>
+                </View>
+                <View style={styles.cardOccupationContainer}>
+                    <Text style={styles.cardOccupation}>
+                        {occupation}
+                    </Text>
+                </View>
+                <View>
+                    <Text style={styles.cardDescription}>
+                        {description}
+                    </Text>
+                </View>
+            </View>
+        </TouchableHighlight>
+    )
+};
+        
+ProfileCard.PropTypes = {
+    image: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    occupation: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    showThumbnail: PropTypes.bool.isRequired,
+    onPress: PropTypes.func.isRequired
+};
 
 export default class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            data: data
+        }
+    }
+
+    handleProfileCardPress = (index) => {
+        const showThumbnail = !this.state.data[index].showThumbnail;
+        this.setState({
+            data: update(this.state.data, {[index]: {showThumbnail: {$set: showThumbnail}}})
+        });
+    };
+
     render() {
+        const list = this.state.data.map(function(item, index) {
+            const { image, name, occupation, description, showThumbnail } = item;
+            return <ProfileCard key={'card-' + index} 
+                        image={image}
+                        name={name}
+                        occupation={occupation}
+                        description={description}
+                        onPress={this.handleProfileCardPress.bind(this, index)}
+                        showThumbnail={showThumbnail}/>
+        }, this);
+
         return (
             <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.cardImageContainer}>
-                        <Image style={styles.cardImage}
-                            source={require('../../assets/images/user.png')} />
-                    </View>
-                    <View>
-                        <Text style={styles.cardName}>
-                            John Doe
-                        </Text>
-                    </View>
-                    <View style={styles.cardOccupationContainer}>
-                        <Text style={styles.cardOccupation}>
-                            React Native Developer
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={styles.cardDescription}>
-                            John is a really great JavaScript developer. He loves
-                            using JS to build React Native applications 
-                            for iOS and Android.
-                        </Text>
-                    </View>
-                </View>
+                {list}
             </View>
         );
     }
 }
 
-const profileCardColor = 'dodgerblue';
+const profileCardColor = 'dodgerblue'
 
 const styles = StyleSheet.create({
     container: {
@@ -49,7 +107,19 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         backgroundColor: profileCardColor,
         width: 300,
-        height: 400
+        height: 400,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {
+                    height: 10
+                },
+                shadowOpacity: 1
+            },
+            android: {
+                elevation: 15
+            }
+        })
     },
     cardImageContainer: {
         alignItems: 'center',
@@ -60,7 +130,21 @@ const styles = StyleSheet.create({
         height: 120,
         borderRadius: 60,
         marginTop: 30,
-        paddingTop: 15
+        paddingTop: 15,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {
+                    height: 10
+                },
+                shadowOpacity: 1
+            },
+            android: {
+                borderWidth: 3,
+                borderColor: 'black',
+                elevation: 15
+            }
+        })
     },
     cardImage: {
         width: 80,
@@ -68,20 +152,31 @@ const styles = StyleSheet.create({
     },
     cardName: {
         color: 'white',
-        marginTop: 30
+        marginTop: 30,
+        textShadowColor: 'black',
+        textShadowOffset: {
+            height: 5,
+            width: 5
+        },
+        textShadowRadius: 3
     },
     cardOccupationContainer: {
         borderColor: 'black',
         borderBottomWidth: 3
     },
     cardOccupation: {
+        fontWeight: 'bold',
         marginTop: 10,
         marginBottom: 10
     },
     cardDescription: {
+        fontStyle: 'italic',
         marginTop: 10,
         marginRight: 40,
         marginLeft: 40,
         marginBottom: 10
+    },
+    cardThumbnail: {
+        transform: [{scale: 0.2}]
     }
 })
